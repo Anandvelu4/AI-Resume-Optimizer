ccb15dff0d663563324e224630380701b45902ef
Anand Velu <40490694-anandwork14@users.noreply.replit.com>
2025-03-16 05:19:54 +0000

    Fixed port binding issue

--- main.py
+++ main.py
@@ -59,7 +59,8 @@ def upload_resume():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0",port=port, debug=True)
 
 # Test fitz
 from pymupdf import fitz
