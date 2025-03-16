ac61397b990da9426e52ac1a813f72111492c918
Anand Velu <40490694-anandwork14@users.noreply.replit.com>
2025-03-16 03:18:43 +0000

    initial commit for render deployment

---
+++ replit.nix
@@ -0,0 +1,24 @@
+{pkgs}: {
+  deps = [
+    pkgs.lsof
+    pkgs.cacert
+    pkgs.which
+    pkgs.bash
+    pkgs.xsimd
+    pkgs.pkg-config
+    pkgs.glibcLocales
+    pkgs.gitFull
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+    pkgs.zlib
+    pkgs.xcodebuild
+  ];
+}
